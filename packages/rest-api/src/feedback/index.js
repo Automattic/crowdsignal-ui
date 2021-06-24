ec09229afdf02c6c742e4016fb04867799b697b3
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-06-24 22:30:22 +0200

    Fix formatting for packages/rest-api

--- packages/rest-api/src/feedback/index.js
+++ packages/rest-api/src/feedback/index.js
@@ -3,17 +3,19 @@
  */
 import { http } from '@crowdsignal/http';
 
-export const fetchFeedbackSurvey = ( surveyId ) => http( {
-	host: 'https://api.crowdsignal.com',
-	path: `/v3/feedback/${ surveyId }`,
-	method: 'GET',
-	mode: 'cors',
-} );
+export const fetchFeedbackSurvey = ( surveyId ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: `/v3/feedback/${ surveyId }`,
+		method: 'GET',
+		mode: 'cors',
+	} );
 
-export const updateFeedbackResponse = ( surveyId, data ) => http( {
-	host: 'https://api.crowdsignal.com',
-	path: `/v3/feedback/${ surveyId }/response`,
-	method: 'POST',
-	mode: 'cors',
-	body: JSON.stringify( data ),
-} );
+export const updateFeedbackResponse = ( surveyId, data ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: `/v3/feedback/${ surveyId }/response`,
+		method: 'POST',
+		mode: 'cors',
+		body: JSON.stringify( data ),
+	} );
