1b771627094e04a3756de4c59ceb670a6ef6dd5f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-06-24 22:42:45 +0200

    Fix formatting for packages/styles

--- packages/styles/src/reset.js
+++ packages/styles/src/reset.js
@@ -11,10 +11,16 @@ import { color } from './colors';
 export const reset = css`
 	:host {
 		color: ${ color( 'text' ) };
-		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+			Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+			'Segoe UI Symbol';
 	}
 
-	button, input, textarea {
-		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";		
+	button,
+	input,
+	textarea {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+			Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+			'Segoe UI Symbol';
 	}
 `;
--- packages/styles/src/util.js
+++ packages/styles/src/util.js
@@ -3,11 +3,11 @@
  * #ff0    -> #ffff00
  * #ff0000 -< #ff0000
  *
- * @param  {String} color Hex color representation
- * @return {String}       Expanded hex color value
+ * @param  {string} color Hex color representation
+ * @return {string}       Expanded hex color value
  */
 const maybePadColor = ( color ) => {
-	let channels = /^#([a-f0-9])([a-f0-9])([a-f0-9])$/i.exec( color );
+	const channels = /^#([a-f0-9])([a-f0-9])([a-f0-9])$/i.exec( color );
 
 	if ( ! channels ) {
 		return color;
@@ -15,20 +15,20 @@ const maybePadColor = ( color ) => {
 
 	channels.shift();
 
-	return '#' + channels
-		.map( ( value ) => value + value )
-		.join( '' );
+	return '#' + channels.map( ( value ) => value + value ).join( '' );
 };
 
 /**
  * Converts a hex color value to rgba()
  *
- * @param  {String} color Hex color representation
- * @param  {Number} alpha Opacity
- * @return {String}       rgba() value
+ * @param  {string} color Hex color representation
+ * @param  {number} alpha Opacity
+ * @return {string}       rgba() value
  */
 export const hexToRGBA = ( color, alpha ) => {
-	let channels = /^#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i.exec( maybePadColor( color ) );
+	let channels = /^#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i.exec(
+		maybePadColor( color )
+	);
 
 	if ( ! channels ) {
 		return '';
@@ -43,8 +43,7 @@ export const hexToRGBA = ( color, alpha ) => {
 /**
  * Converts a hex color value to rgb()
  *
- * @param  {String} color Hex color representation
- * @return {String}       rgb() value
+ * @param  {string} color Hex color representation
+ * @return {string}       rgb() value
  */
-export const hexToRGB = ( color ) =>
-	hexToRGBA( color, 1 );
+export const hexToRGB = ( color ) => hexToRGBA( color, 1 );
