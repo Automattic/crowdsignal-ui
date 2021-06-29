f4ed301a05e4f06bb2d2adb78251c103525ceeb5
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-06-29 21:30:06 +0200

    Initialize @crowdsignal/block-editor package

---
+++ packages/block-editor/README.md
@@ -0,0 +1,3 @@
+# @crowdsignal/block-editor
+
+This package contains all the common code shared between Crowdsignal's editor instances.
---
+++ packages/block-editor/src/stories/index.js
@@ -0,0 +1,11 @@
+/**
+ * Internal dependencies
+ */
+import { BlockEditor } from '../';
+
+export default {
+	title: 'Block Editor/Editor',
+	component: BlockEditor,
+};
+
+export const Editor = () => <BlockEditor />;
