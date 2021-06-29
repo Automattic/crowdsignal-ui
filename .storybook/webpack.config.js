4247f1635ae5b6ba2b7968fa54dae20ae4db084f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-06-29 21:32:10 +0200

    Update storybook configuration to support the block editor

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,18 @@
-module.exports = ( { config } ) => {
-	config.module.rules.push(
-		{
-			test: /\stories\/.+\.js$/,
-			loader: require.resolve( '@storybook/source-loader' ),
-			enforce: 'pre',
-		}
-	);
-
-	return config;
-};
+module.exports = ( { config } ) => ( {
+	...config,
+	module: {
+		...config.module,
+		rules: [
+			...config.module.rules,
+			{
+				test: /stories\/.+\.js$/,
+				loader: require.resolve( '@storybook/source-loader' ),
+				enforce: 'pre',
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+} );
