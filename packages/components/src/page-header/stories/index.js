b5aa31217e50b93fa6a54dc9bc1f9ee0c6b4c84c
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-07-07 17:07:27 +0200

    Add a <PageHeader /> component

---
+++ packages/components/src/page-header/stories/index.js
@@ -0,0 +1,14 @@
+/**
+ * Internal dependencies
+ */
+import PageHeader from '../';
+
+export default {
+	title: 'Components/Page Header',
+	component: PageHeader,
+};
+
+const Default = () => (
+	<PageHeader>The quick brown fox jumps over the lazy dog</PageHeader>
+);
+export { Default as PageHeader };
