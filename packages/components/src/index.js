697846fa02bf2bad5dc9e58c4a1d60200bc59f9d
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-07-07 23:45:46 +0200

    Update components index

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './button';
 export { default as CrowdsignalFooter } from './crowdsignal-footer';
 export { default as Popover } from './popover';
+export { default as PopoverMenu } from './popover-menu';
 export { default as StyleProvider } from './style-provider';
