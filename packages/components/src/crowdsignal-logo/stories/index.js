8bf2951df74b356e30bb491cf8f6fe72baac3f04
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-07-07 18:48:07 +0200

    Add a <CrowdsignalLogo /> component

---
+++ packages/components/src/crowdsignal-logo/stories/index.js
@@ -0,0 +1,13 @@
+/**
+ * Internal dependencies
+ */
+import CrowdsignalLogo from '../';
+
+export default {
+	title: 'Components/Crowdsignal Logo',
+	component: CrowdsignalLogo,
+};
+
+const Default = () => <CrowdsignalLogo size={ 144 } />;
+
+export { Default as CrowdsignalLogo };
