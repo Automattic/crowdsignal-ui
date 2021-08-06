c879149cd07e414f32d977c52e9fc870ba23c994
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-08-06 15:38:58 +0200

    Implement a `redirect()` method for @crowdsignal/router

--- packages/router/src/index.js
+++ packages/router/src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Internal dependencies
+ */
+import { history } from './context';
+
 export * from './route';
 export * from './router';
 export * from './switch';
+
+export const redirect = ( uri ) => history.push( uri );
