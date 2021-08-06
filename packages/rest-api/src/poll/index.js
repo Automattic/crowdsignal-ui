4351c8c03a936e5d96961923fcc4e07c8a3cf53b
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-08-06 15:46:16 +0200

    Add handlers for Poll API

---
+++ packages/rest-api/src/poll/index.js
@@ -0,0 +1,36 @@
+/**
+ * Internal dependencies
+ */
+import { http } from '@crowdsignal/http';
+
+export const fetchPoll = ( pollId ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: `/v4/polls/${ pollId }`,
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include',
+	} );
+
+export const createPoll = ( data ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: '/v4/polls',
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		body: JSON.stringify( data ),
+	} );
+
+export const updatePoll = ( pollId, data ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: `/v4/polls/${ pollId }`,
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		headers: {
+			'X-HTTP-Method-Override': 'PATCH',
+		},
+		body: JSON.stringify( data ),
+	} );
