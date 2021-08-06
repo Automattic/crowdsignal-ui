f92dd0a04b4be0437657fdf8aae1675d0fd6e4e8
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-08-06 15:46:33 +0200

    Add handlers for the project API

---
+++ packages/rest-api/src/project/index.js
@@ -0,0 +1,36 @@
+/**
+ * Internal dependencies
+ */
+import { http } from '@crowdsignal/http';
+
+export const fetchProject = ( projectId ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: `/v4/projects/${ projectId }`,
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include',
+	} );
+
+export const createProject = ( data ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: '/v4/projects',
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		body: JSON.stringify( data ),
+	} );
+
+export const updateProject = ( projectId, data ) =>
+	http( {
+		host: 'https://api.crowdsignal.com',
+		path: `/v4/projects/${ projectId }`,
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		headers: {
+			'X-Http-Method-Override': 'PATCH',
+		},
+		body: JSON.stringify( data ),
+	} );
