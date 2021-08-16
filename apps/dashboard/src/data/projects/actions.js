c9f9d06ed7ee5b07ef593aafc8a439bea59d4db8
Christian Gastrell <cgastrell@gmail.com>
2021-08-16 19:04:59 -0300

    test: use project action to yield multiple cross-state actions. Redirect still not doing it

--- apps/dashboard/src/data/projects/actions.js
+++ apps/dashboard/src/data/projects/actions.js
@@ -1,7 +1,9 @@
 /**
  * Internal dependencies
  */
+
 import { PROJECT_SAVE, PROJECT_UPDATE } from '../action-types';
+import { fetchOngoing, fetchIdle, redirect } from 'data/ui/actions';
 
 export function saveProject( projectId, project ) {
 	return {
@@ -21,13 +23,21 @@ export function updateProject( projectId, project ) {
 
 export function* saveAndUpdateProject( projectId, project ) {
 	try {
+		yield fetchOngoing();
 		const response = yield saveProject( projectId, project );
 		const id = projectId || response.data.id;
 
-		return updateProject( id, {
+		yield updateProject( id, {
 			...project,
 			id,
 		} );
+
+		if ( ! projectId ) {
+			// this should only happen when creating a new project
+			yield redirect( `/${ id }` );
+		}
+
+		return fetchIdle();
 	} catch ( error ) {
 		// Save failed
 		throw error;
