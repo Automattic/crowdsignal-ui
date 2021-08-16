f63412fec8cd24570d77480573f4921b1247f292
Christian Gastrell <cgastrell@gmail.com>
2021-08-16 18:54:43 -0300

    comment out all that code while we figure out the flow

--- apps/dashboard/src/components/editor/index.js
+++ apps/dashboard/src/components/editor/index.js
@@ -1,19 +1,19 @@
 /**
  * External dependencies
  */
-import { useCallback, useEffect } from '@wordpress/element';
-import { useDispatch, select } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
-import { debounce } from 'lodash';
+// import { useCallback, useEffect } from '@wordpress/element';
+// import { useDispatch, select } from '@wordpress/data';
+// import { __ } from '@wordpress/i18n';
+// import { debounce } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { BlockEditor } from '@crowdsignal/block-editor';
 import ProjectNavigation from '../project-navigation';
-import { STORE_NAME } from '../../data';
 import { registerBlocks } from './blocks';
-import EditorLoadingPlaceholder from './loading-placeholder';
+// import { STORE_NAME } from 'data';
+// import EditorLoadingPlaceholder from './loading-placeholder';
 
 /**
  * Style dependencies
@@ -21,46 +21,47 @@ import EditorLoadingPlaceholder from './loading-placeholder';
 import './style.scss';
 
 const Editor = ( { projectId } ) => {
-	const { redirect, saveAndUpdateProject } = useDispatch( STORE_NAME );
+	// const { redirect, saveAndUpdateProject } = useDispatch( STORE_NAME );
 
-	useEffect( () => {
-		if ( projectId ) {
-			return;
-		}
+	// useEffect( () => {
+	// 	if ( projectId ) {
+	// 		return;
+	// 	}
+	//
+	// 	try {
+	// 		saveAndUpdateProject( 0, {
+	// 			title: __( 'Untitled Project', 'dashboard' ),
+	// 		} );
+	//
+	// 		const id = select( STORE_NAME ).getLastUpdatedProjectId();
+	//
+	// 		redirect( `/edit/poll${ id }` );
+	// 	} catch ( error ) {
+	// 		// Creating a new project failed, return to the main dashboard screen and display an error.
+	// 		// redirect() doesn't work as it's not part of the app yet.
+	// 		console.error( error );
+	// 		// window.location.replace( 'https://app.crowdsignal.com/dashboard' );
+	// 	}
+	// }, [] );
 
-		try {
-			saveAndUpdateProject( 0, {
-				title: __( 'Untitled Project', 'dashboard' ),
-			} );
+	// const handleSaveBlocks = useCallback(
+	// 	debounce( ( blocks ) => {
+	// 		try {
+	// 			saveAndUpdateProject( projectId, {
+	// 				blocks,
+	// 			} );
+	// 		} catch ( error ) {
+	// 			// console.error( error );
+	// 			// console.error( 'Failed to save project content.' );
+	// 		}
+	// 	}, 1000 ),
+	// 	[ projectId, saveAndUpdateProject ]
+	// );
+	const handleSaveBlocks = () => {};
 
-			const id = select( STORE_NAME ).getLastUpdatedProjectId();
-
-			redirect( `/edit/poll${ id }` );
-		} catch ( error ) {
-			// Creating a new project failed, return to the main dashboard screen and display an error.
-			// redirect() doesn't work as it's not part of the app yet.
-
-			window.location.replace( 'https://app.crowdsignal.com/dashboard' );
-		}
-	}, [] );
-
-	const handleSaveBlocks = useCallback(
-		debounce( ( blocks ) => {
-			try {
-				saveAndUpdateProject( projectId, {
-					blocks,
-				} );
-			} catch ( error ) {
-				// console.error( error );
-				// console.error( 'Failed to save project content.' );
-			}
-		}, 1000 ),
-		[ projectId, saveAndUpdateProject ]
-	);
-
-	if ( ! projectId ) {
-		return <EditorLoadingPlaceholder />;
-	}
+	// if ( ! projectId ) {
+	// 	return <EditorLoadingPlaceholder />;
+	// }
 
 	return (
 		<div className="editor">
