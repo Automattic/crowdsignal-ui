32bd1efcf5ea073f1fa2bf62c557c9831cd3ce5c
Christian Gastrell <cgastrell@gmail.com>
2021-08-19 18:35:44 -0300

    get rid of unsused code

--- apps/dashboard/src/data/ui/controls.js
+++ apps/dashboard/src/data/ui/controls.js
@@ -1,13 +1,1 @@
-/**
- * Internal dependencies
- */
-import { redirect } from '@crowdsignal/router';
-import { ROUTE_UPDATE } from '../action-types';
-
-export default {
-	[ ROUTE_UPDATE ]: ( { path } ) => {
-		// eslint-disable-next-line
-		console.log( 'control', ROUTE_UPDATE );
-		redirect( path );
-	},
-};
+export default {};
