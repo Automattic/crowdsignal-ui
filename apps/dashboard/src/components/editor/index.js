6dc486d2c6a9a0a9c8a684f326a8cccbc8f90447
Christian Gastrell <cgastrell@gmail.com>
2021-09-03 17:12:24 -0300

    Use a BlockLoader component to receive the loaded blocks and inject them on the editor. Only BlockEditor children can access the `isolated/editor` store.

--- apps/dashboard/src/components/editor/index.js
+++ apps/dashboard/src/components/editor/index.js
@@ -4,7 +4,7 @@
 import { useCallback } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 // import { __ } from '@wordpress/i18n';
-import { debounce } from 'lodash';
+import { debounce, get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -14,6 +14,7 @@ import ProjectNavigation from '../project-navigation';
 import { STORE_NAME } from '../../data';
 import { registerBlocks } from './blocks';
 import EditorLoadingPlaceholder from './loading-placeholder';
+import BlockLoader from './block-loader';
 
 /**
  * Style dependencies
@@ -24,6 +25,18 @@ const Editor = ( { projectId } ) => {
 	const project = useSelect( ( select ) =>
 		select( STORE_NAME ).getProject( projectId )
 	);
+
+	const projectContent = get( project, [ 'content' ], {} );
+
+	// TODO: need to compare draft/published, offer "restore" drafted content
+	const draftedBlocks = get( projectContent, [ 'draft', 'pages', 0 ], [] );
+
+	const displayedBlocks = get(
+		projectContent,
+		[ 'published', 'pages', 0 ],
+		draftedBlocks
+	);
+
 	const { saveAndUpdateProject } = useDispatch( STORE_NAME );
 
 	const handleSaveBlocks = useCallback(
@@ -62,7 +75,9 @@ const Editor = ( { projectId } ) => {
 				projectId={ projectId }
 			/>
 
-			<BlockEditor onSave={ handleSaveBlocks } />
+			<BlockEditor onSave={ handleSaveBlocks }>
+				<BlockLoader blocks={ displayedBlocks } />
+			</BlockEditor>
 		</div>
 	);
 };
