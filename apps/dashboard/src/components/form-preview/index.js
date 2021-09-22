20f3a35223c45ea16ecaa289060fc91efebd9033
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-09-22 22:54:39 +0200

    Implement a simple preview component

---
+++ apps/dashboard/src/components/form-preview/index.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import { useSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { renderBlocks } from '@crowdsignal/blocks';
+import { STORE_NAME } from '../../data';
+
+const FormPreview = ( { projectId } ) => {
+	const project = useSelect( ( select ) =>
+		select( STORE_NAME ).getProject( projectId )
+	);
+
+	if ( ! project?.content?.draft ) {
+		return null;
+	}
+
+	return renderBlocks( project.content.draft.pages[ 0 ], {
+		'crowdsignal-forms/poll': ( { children } ) => (
+			<div className="poll">{ children }</div>
+		),
+		'crowdsignal-forms/poll-answer': () => (
+			<div className="poll-answer">Answer Block</div>
+		),
+		'crowdsignal-forms/free-text': () => <div className="free-text" />,
+		'crowdsignal-forms/multiple-choice': () => (
+			<div className="multiple-choice" />
+		),
+	} );
+};
+
+export default FormPreview;
