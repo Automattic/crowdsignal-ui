71dd92c91034d2f6175d736566f031295cfd26a8
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-09-24 22:24:34 +0200

    Add a temporary content wrapper for the preview

--- apps/dashboard/src/components/form-preview/index.js
+++ apps/dashboard/src/components/form-preview/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { useSelect } from '@wordpress/data';
+import styled from '@emotion/styled';
 
 /**
  * Internal dependencies
@@ -15,6 +16,12 @@ import {
 } from '@crowdsignal/blocks';
 import { STORE_NAME } from '../../data';
 
+const ContentWrapper = styled.div`
+	margin: 0 auto;
+	max-width: 720px;
+	padding: 20px;
+`;
+
 const FormPreview = ( { projectId } ) => {
 	const project = useSelect( ( select ) =>
 		select( STORE_NAME ).getProject( projectId )
@@ -24,12 +31,16 @@ const FormPreview = ( { projectId } ) => {
 		return null;
 	}
 
-	return renderBlocks( project.content.draft.pages[ 0 ], {
-		'crowdsignal-forms/poll': Poll,
-		'crowdsignal-forms/poll-answer': PollAnswer,
-		'crowdsignal-forms/free-text': FreeText,
-		'crowdsignal-forms/multiple-choice': MultipleChoice,
-	} );
+	return (
+		<ContentWrapper>
+			{ renderBlocks( project.content.draft.pages[ 0 ], {
+				'crowdsignal-forms/poll': Poll,
+				'crowdsignal-forms/poll-answer': PollAnswer,
+				'crowdsignal-forms/free-text': FreeText,
+				'crowdsignal-forms/multiple-choice': MultipleChoice,
+			} ) }
+		</ContentWrapper>
+	);
 };
 
 export default FormPreview;
