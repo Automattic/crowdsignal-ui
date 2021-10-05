5cba1a6824795ef32ae97c1a1f3436170617844f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-10-05 17:23:25 +0200

    Add FormTextInput and FormTextarea block components

---
+++ packages/blocks/src/components/form-textarea/index.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import styled from '@emotion/styled';
+
+const FormTextarea = styled.textarea`
+	border-style: solid;
+	box-sizing: border-box;
+	width: 100%;
+`;
+
+export default FormTextarea;
--- packages/blocks/src/components/index.js
+++ packages/blocks/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from './button';
+export { default as FormTextarea } from './form-textarea';
 export { default as QuestionWrapper } from './question-wrapper';
