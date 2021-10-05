1783722dd642f0aec46eb804ea5659acf736a35f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-10-05 17:24:05 +0200

    Add an is-selected modifier to the blocks' <Button> component

--- packages/blocks/src/components/button/index.js
+++ packages/blocks/src/components/button/index.js
@@ -9,9 +9,17 @@ import classnames from 'classnames';
  */
 import { useColorStyles } from '@crowdsignal/styles';
 
+const StyledButtonWrapper = styled.div`
+	&.is-selected {
+		filter: invert( 1 );
+	}
+`;
+
 const StyledButton = styled.button`
 	border: 0;
 	cursor: pointer;
+	overflow: hidden;
+	position: relative;
 
 	&.rich-text {
 		cursor: text;
@@ -25,7 +33,9 @@ const Button = ( {
 	style = {},
 	...props
 } ) => (
-	<div className={ classnames( className, 'wp-block-button' ) }>
+	<StyledButtonWrapper
+		className={ classnames( className, 'wp-block-button' ) }
+	>
 		<StyledButton
 			className="wp-block-button__link"
 			style={ {
@@ -36,7 +46,7 @@ const Button = ( {
 		>
 			{ children }
 		</StyledButton>
-	</div>
+	</StyledButtonWrapper>
 );
 
 export default Button;
