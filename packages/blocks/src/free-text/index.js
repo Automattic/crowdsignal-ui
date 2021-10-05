e638a81a64b999fc8342e5e32b5177b9395901d1
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-10-05 17:22:52 +0200

    Update free text block to store the value inside the form

--- packages/blocks/src/free-text/index.js
+++ packages/blocks/src/free-text/index.js
@@ -6,14 +6,19 @@ import { RichText } from '@wordpress/block-editor';
 /**
  * Internal dependencies
  */
-import { QuestionWrapper } from '../components';
+import { useField } from '@crowdsignal/form';
+import { FormTextarea, QuestionWrapper } from '../components';
 
 const FreeText = ( { attributes, className } ) => {
+	const { inputProps } = useField( {
+		name: `q_${ attributes.id }`,
+	} );
+
 	return (
 		<QuestionWrapper attributes={ attributes } className={ className }>
 			<RichText.Content tagName="h3" value={ attributes.question } />
 
-			<textarea rows={ 6 } />
+			<FormTextarea { ...inputProps } rows={ 6 } />
 		</QuestionWrapper>
 	);
 };
