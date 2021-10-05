cb7fbf43a638a7bff106ca53a8fbed126be5d242
Christian Gastrell <cgastrell@gmail.com>
2021-10-05 15:58:31 -0300

    remove form preview component, use new answer components and submit button on renderer

--- apps/project-renderer/src/components/app/index.js
+++ apps/project-renderer/src/components/app/index.js
@@ -1,13 +1,50 @@
 /**
+ * External dependencies
+ */
+import styled from '@emotion/styled';
+
+/**
  * Internal dependencies
  */
-import FormPreview from '../form-preview';
+import {
+	Answer,
+	FreeText,
+	MultipleChoice,
+	Poll,
+	PollAnswer,
+	SubmitButton,
+	renderBlocks,
+} from '@crowdsignal/blocks';
+import { Form } from '@crowdsignal/form';
+
+// TODO: this is just to make the render look good, selected theme should take care of this?
+const ContentWrapper = styled.div`
+	margin: 0 auto;
+	max-width: 720px;
+	padding: 20px;
+`;
 
 const App = ( { project } ) => {
 	const content = project.content.published.pages[ 0 ];
+	const projectId = project.id;
+
+	// eslint-disable-next-line no-console
+	const handleSubmit = ( data ) => console.log( data );
+
 	return (
 		<div className="app">
-			<FormPreview content={ content } />
+			<ContentWrapper>
+				<Form name={ `f-${ projectId }` } onSubmit={ handleSubmit }>
+					{ renderBlocks( content, {
+						'crowdsignal-forms/answer': Answer,
+						'crowdsignal-forms/free-text': FreeText,
+						'crowdsignal-forms/multiple-choice': MultipleChoice,
+						'crowdsignal-forms/poll': Poll,
+						'crowdsignal-forms/poll-answer': PollAnswer,
+						'crowdsignal-forms/submit-button': SubmitButton,
+					} ) }
+				</Form>
+			</ContentWrapper>
 		</div>
 	);
 };
