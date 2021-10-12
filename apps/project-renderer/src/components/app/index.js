e5e732e8393572bb4757a27a8b42d560d18e9998
Christian Gastrell <cgastrell@gmail.com>
2021-10-12 12:33:53 -0300

    Update project renderer app (#61)
    * update to a fixed project layout, only for debugging purposes
    
    * The component now accepts/requires more props, we might consider consolidating all these into a single one
    
    * basic form handler and submit: data is sent and result is shown on console

--- apps/project-renderer/src/components/app/index.js
+++ apps/project-renderer/src/components/app/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import styled from '@emotion/styled';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -24,12 +25,45 @@ const ContentWrapper = styled.div`
 	padding: 20px;
 `;
 
-const App = ( { project } ) => {
-	const content = project.content.published.pages[ 0 ];
+const App = ( { project, page = 0, responseHash = '', startDate = 0 } ) => {
+	// page on backend starts at ID = 1, try to substract or default to 0
+	page = get( project, [ 'content', 'published', 'pages', page - 1 ] )
+		? page - 1
+		: 0;
+	const content = project.content.published.pages[ page ];
 	const projectId = project.id;
 
-	// eslint-disable-next-line no-console
-	const handleSubmit = ( data ) => console.log( data );
+	const handleSubmit = ( data ) => {
+		if ( ! data ) {
+			return;
+		}
+		const form = new window.FormData();
+		form.append( 'p', page );
+		form.append( 'r', responseHash );
+		form.append( 'startTime', startDate || parseInt( Date.now() / 1000 ) );
+		Object.keys( data ).forEach( ( key ) =>
+			form.append( key, data[ key ] )
+		);
+
+		window
+			.fetch(
+				`https://api.crowdsignal.com/v4/projects/${ projectId }/responses`,
+				{
+					method: 'POST',
+					body: form,
+				}
+			)
+			.then( ( res ) => {
+				if ( ! res.ok ) {
+					throw new Error( res.status );
+				}
+				return res.json();
+			} )
+			// eslint-disable-next-line no-console
+			.catch( ( err ) => console.error( err ) )
+			// eslint-disable-next-line no-console
+			.then( ( json ) => console.log( json ) );
+	};
 
 	return (
 		<div className="app">
