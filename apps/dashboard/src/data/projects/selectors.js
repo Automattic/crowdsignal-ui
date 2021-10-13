0a6f835c0fc7a6200d5125c1e3a09bd99285136f
Christian Gastrell <cgastrell@gmail.com>
2021-10-13 18:09:52 -0300

    Add getProjectStatus, isProjectPublic and isProjectSaved selectors

--- apps/dashboard/src/data/projects/selectors.js
+++ apps/dashboard/src/data/projects/selectors.js
@@ -1,7 +1,12 @@
 /**
  * External dependencies
  */
-import { get } from 'lodash';
+import { get, includes } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import * as ProjectStatus from '../../util/project/project-status';
 
 export const getLastUpdatedProjectId = ( state ) =>
 	get( state, [ 'projects', 'lastUpdatedItemId' ], 0 );
@@ -12,8 +17,25 @@ export const getProject = ( state, projectId ) =>
 export const isProjectSaving = ( state ) =>
 	get( state, [ 'projects', 'isSaving' ], false );
 
-export const isProjectDraftSaved = ( state ) =>
-	get( state, [ 'projects', 'isDraftSaved' ], false );
+export const getProjectStatus = ( state ) =>
+	get(
+		state,
+		[ 'projects', 'projectStatus' ],
+		ProjectStatus.DRAFT_WITH_UNSAVED_CHANGES
+	);
 
-export const isProjectPublishSaved = ( state ) =>
-	get( state, [ 'projects', 'isPublishSaved' ], false );
+export const isProjectPublic = ( state ) =>
+	includes(
+		[
+			ProjectStatus.PUBLIC,
+			ProjectStatus.PUBLIC_WITH_UNSAVED_CHAGES,
+			ProjectStatus.PUBLIC_WITH_UNPUBLISHED_CHAGES,
+		],
+		getProjectStatus( state )
+	);
+
+export const isProjectSaved = ( state ) =>
+	includes(
+		[ ProjectStatus.DRAFT, ProjectStatus.PUBLIC ],
+		getProjectStatus( state )
+	);
