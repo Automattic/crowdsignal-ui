d6116b6d3ab2bdad9146dd4254533980be21a861
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-10-14 22:26:30 +0200

    Add a new @crowdsignal/icons package

---
+++ packages/icons/src/text-question.js
@@ -0,0 +1,14 @@
+export const TextQuestionIcon = () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M6 9.75H18C18.6904 9.75 19.25 10.3096 19.25 11V17C19.25 17.6904 18.6904 18.25 18 18.25H6C5.30964 18.25 4.75 17.6904 4.75 17V11C4.75 10.3096 5.30964 9.75 6 9.75Z"
+			strokeWidth="1.5"
+		/>
+		<path d="M4 5H20V6.5H4V5Z" />
+	</svg>
+);
