3561d36e20d7c08c701655e040f0516c86f806df
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-11-04 12:09:08 +0100

    Use block editor data to count submit button blocks for the current page

--- apps/dashboard/src/components/editor/publish-button.js
+++ apps/dashboard/src/components/editor/publish-button.js
@@ -4,7 +4,6 @@
 import { Button } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { isEmpty } from 'lodash';
 
 /**
  * Internal dependencies
@@ -13,40 +12,21 @@ import { submitButtonBlock } from '@crowdsignal/block-editor';
 import { STORE_NAME } from '../../data';
 import { hasUnpublishedChanges } from '../../util/project';
 
-const includesSubmitButton = ( blocks ) => {
-	for ( const block of blocks ) {
-		if ( block.name === submitButtonBlock.name ) {
-			return true;
-		}
-
-		if (
-			! isEmpty( block.innerBlocks ) &&
-			includesSubmitButton( block.innerBlocks )
-		) {
-			return true;
-		}
-	}
-
-	return false;
-};
-
-const hasMissingSubmitButtons = ( project ) => {
-	for ( const page of project?.content?.draft?.pages ) {
-		if ( ! includesSubmitButton( page ) ) {
-			return true;
-		}
-	}
-
-	return false;
-};
-
 const PublishButton = ( { projectId } ) => {
 	const { saveAndUpdateProject } = useDispatch( STORE_NAME );
 
-	const [ project, isSaving, isPublic ] = useSelect( ( select ) => [
+	const [
+		project,
+		isSaving,
+		isPublic,
+		currentPageSubmitButtonCount,
+	] = useSelect( ( select ) => [
 		select( STORE_NAME ).getProject( projectId ),
 		select( STORE_NAME ).isProjectSaving(),
 		select( STORE_NAME ).isProjectPublic(),
+		select( 'core/block-editor' ).getGlobalBlockCount(
+			submitButtonBlock.name
+		),
 	] );
 
 	const publishProject = () => {
@@ -72,7 +52,7 @@ const PublishButton = ( { projectId } ) => {
 			className="is-crowdsignal"
 			variant={ isPublic ? 'tertiary' : 'primary' }
 			onClick={ publishProject }
-			disabled={ isSaving || hasMissingSubmitButtons( project ) }
+			disabled={ isSaving || currentPageSubmitButtonCount < 1 }
 		>
 			{ isPublic
 				? __( 'Update', 'dashboard' )
