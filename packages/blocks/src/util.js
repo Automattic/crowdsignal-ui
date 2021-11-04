072ff16bfa934eb0bca2da2433d99f2f911eb1d6
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-11-04 15:54:06 +0100

    Update getBlockStyle to deal with any className string

--- packages/blocks/src/util.js
+++ packages/blocks/src/util.js
@@ -4,5 +4,9 @@
  * @param  {string} className Block class name
  * @return {string}           Block style
  */
-export const getBlockStyle = ( className ) =>
-	className.match( /is-style-([^\s]+)/i )[ 1 ];
+export const getBlockStyle = ( className ) => {
+	const styleClass =
+		className && className.match( /is-style-([^\s]+)/i )[ 1 ];
+
+	return styleClass ? styleClass[ 1 ] : '';
+};
