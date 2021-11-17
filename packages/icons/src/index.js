c6cb49dc0d6f3c25c52b15eba5ea17b6c7a9abeb
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-11-17 19:09:46 +0100

    Add multiple choice answer and submit button icons

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -1,5 +1,7 @@
 export * from './multiple-choice';
+export * from './multiple-choice-answer';
 export * from './multiple-choice-question';
 export * from './single-choice';
+export * from './submit-button';
 export * from './text-input';
 export * from './text-question';
