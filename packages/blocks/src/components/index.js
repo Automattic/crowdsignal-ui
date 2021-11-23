ecfa7806060d6fdae92618b310745490f7e1cf88
João Heringer <joao.heringer@automattic.com>
2021-11-23 10:01:30 -0300

    Export FormTextInput from blocks package

--- packages/blocks/src/components/index.js
+++ packages/blocks/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './button';
 export { default as FormCheckbox } from './form-checkbox';
 export { default as FormTextarea } from './form-textarea';
+export { default as FormTextInput } from './form-text-input';
 export { default as QuestionHeader } from './question-header';
 export { default as QuestionWrapper } from './question-wrapper';
