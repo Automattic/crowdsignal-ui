4de9fbcbdd63042c43dedb3c4320ad936ed53c34
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-11-29 16:44:22 +0100

    List optional chaining plugin explicitly in babel config

--- apps/dashboard/babel.config.js
+++ apps/dashboard/babel.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	],
 	plugins: [
 		'@babel/plugin-transform-runtime',
+		'@babel/plugin-proposal-optional-chaining',
 		'@wordpress/babel-plugin-import-jsx-pragma',
 		'babel-plugin-emotion',
 	],
