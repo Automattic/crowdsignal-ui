c18e71d90f228a25ff31569c443eb06f9a19d95e
Christian Gastrell <cgastrell@gmail.com>
2021-12-01 18:54:10 -0300

    Add is-disabled class. Add class for prop isDisabled

--- packages/components/src/tab-navigation/tab.js
+++ packages/components/src/tab-navigation/tab.js
@@ -3,11 +3,12 @@
  */
 import classnames from 'classnames';
 
-const Tab = ( { children, href, isSelected, ...props } ) => {
+const Tab = ( { children, href, isSelected, isDisabled, ...props } ) => {
 	const ButtonComponent = href ? 'a' : 'button';
 
 	const classes = classnames( 'tab-navigation__item', {
 		'is-selected': isSelected,
+		'is-disabled': isDisabled,
 	} );
 
 	return (
