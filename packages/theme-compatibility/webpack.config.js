c2e41f311480dc65d8a9827ca4a794e36240d277
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-12-02 18:46:57 +0100

    Set up a @crowdsignal/theme-compatibility package

---
+++ packages/theme-compatibility/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require( 'path' );
+const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		leven: './stylesheets/leven.scss',
+	},
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				type: 'asset/resource',
+				generator: {
+					filename: '[name].min.css',
+				},
+				use: [
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new FixStyleOnlyEntriesPlugin(),
+	]
+};
