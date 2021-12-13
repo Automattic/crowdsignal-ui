03e7c5a010e6207bcae1d98cf2c488192cdfe89f
João Heringer <joao.heringer@automattic.com>
2021-12-12 23:42:26 -0300

    Update spacing values

--- packages/blocks/src/components/form-textarea/index.js
+++ packages/blocks/src/components/form-textarea/index.js
@@ -21,7 +21,7 @@ const FormTextarea = styled.textarea`
 	border-width: 1px;
 	box-sizing: border-box;
 	width: 100%;
-	padding: 7px;
+	padding: 8px;
 `;
 
 FormTextarea.Preview = ( { style, ...props } ) => (
