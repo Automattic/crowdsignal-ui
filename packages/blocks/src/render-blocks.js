5bb0906b50d496296dfaedc906b74e2811b0dd4e
João Heringer <joao.heringer@automattic.com>
2021-12-23 14:42:38 -0300

    Revert render-blocks.js changes

--- packages/blocks/src/render-blocks.js
+++ packages/blocks/src/render-blocks.js
@@ -109,7 +109,7 @@ const appendInnerBlocks = ( blockName, elements, innerBlocks ) => {
  */
 const createElementsRecursive = ( elements ) => {
 	if ( isEmpty( elements ) ) {
-		return [];
+		return null;
 	}
 
 	return map( elements, ( element ) => {
@@ -117,18 +117,13 @@ const createElementsRecursive = ( elements ) => {
 			return element;
 		}
 
-		const children = [
-			...( element.innerBlocks || [] ),
-			...createElementsRecursive( element.children ),
-		];
-
 		return createElement(
 			element.name,
 			element.props,
 
 			// In practice, if an element is the host for inner blocks
 			// it's children should alwyas be empty here.
-			( children.length && children ) || null
+			element.innerBlocks || createElementsRecursive( element.children )
 		);
 	} );
 };
