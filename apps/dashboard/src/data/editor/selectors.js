e080468f7d73d1fb43602e5901df3279ef26b425
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-12-28 16:51:52 +0100

    Split editor state from project state in dashboard data

---
+++ apps/dashboard/src/data/editor/selectors.js
@@ -0,0 +1,10 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+export const isEditorContentSaved = ( state ) =>
+	! get( state, [ 'editor', 'hasUnsavedChanges' ], false );
+
+export const isEditorSaving = ( state ) =>
+	get( state, [ 'editor', 'isSaving' ], false );
