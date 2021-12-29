64c67fc06306dbaeacb6f60bb4cbdefb6ce1ec32
jcheringer <jcheringer@users.noreply.github.com>
2021-12-29 11:36:08 -0300

    Fix Image block is not displaying the image on Project Renderer (#102)
    * Update render-blocks.js to handle element innerBlocks and children
    
    * Revert render-blocks.js changes
    
    * Handle core/cover block to add children inside the container
    
    * Remove commented code

--- packages/blocks/src/render-blocks.js
+++ packages/blocks/src/render-blocks.js
@@ -14,6 +14,7 @@ import {
 	map,
 	uniqueId,
 	zipObject,
+	find,
 } from 'lodash';
 
 const KEY_PREFIX = 'crowdsignal-block-';
@@ -86,16 +87,16 @@ const appendInnerBlocks = ( blockName, elements, innerBlocks ) => {
 	}
 
 	switch ( blockName ) {
-		// case 'core/media-text':
-		// 	const container = find(
-		// 		elements[0].children,
-
-		// 		( element ) => element.props.className.indexOf( 'wp-block-media-text__content' ) >= 0
-		// 	);
-
-		// 	container.innerBlocks = innerBlocks;
-		// 	return;
-
+		case 'core/cover':
+			const container = find(
+				elements[ 0 ].children,
+				( element ) =>
+					element.props.className.indexOf(
+						'wp-block-cover__inner-container'
+					) >= 0
+			);
+			container.innerBlocks = innerBlocks;
+			break;
 		default:
 			elements[ 0 ].innerBlocks = innerBlocks;
 	}
