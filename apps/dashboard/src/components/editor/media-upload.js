8be2de27fc0e0bb2f7d09bcdc3e45834d10a0680
Christian Gastrell <cgastrell@gmail.com>
2022-01-28 12:02:14 -0300

    Use filters to switch MediaUpload for an empty component. Add filter to append linkDestination to core/image blocks

---
+++ apps/dashboard/src/components/editor/media-upload.js
@@ -0,0 +1,18 @@
+/**
+ * This is (for now) a mocking component. Invoked through a filter to render right next to the Upload button.
+ *
+ * Refs:
+ * - ./blocks.js
+ * - https://github.com/WordPress/gutenberg/tree/trunk/packages/block-editor/src/components/media-placeholder
+ * - https://github.com/WordPress/gutenberg/tree/trunk/packages/block-editor/src/components/media-upload
+ */
+export const MediaUploader = () => {
+	// const open = () => console.log( 'should spawn some media library modal' );
+	// return (
+	// 	<div>
+	// 		<Button onClick={ open }>Open Media Library</Button>
+	// 	</div>
+	// );
+
+	return null;
+};
