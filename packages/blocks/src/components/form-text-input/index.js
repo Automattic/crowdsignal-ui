6be3e899286d29a93cd0384163a8585a616df3bf
João Heringer <joao.heringer@automattic.com>
2022-02-03 16:00:15 -0300

    Force block border style when in error state

--- packages/blocks/src/components/form-text-input/index.js
+++ packages/blocks/src/components/form-text-input/index.js
@@ -22,7 +22,7 @@ const Input = styled.input`
 	box-sizing: border-box;
 
 	${ FormInputWrapper }.is-error & {
-		border-color: var( --color-error );
+		border: 1px solid var( --color-error ) !important;
 	}
 `;
 
