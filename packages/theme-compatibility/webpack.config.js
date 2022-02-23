47fe4ecfd9249af04948252c6c25be4b533dc477
jcheringer <jcheringer@users.noreply.github.com>
2022-02-23 17:29:27 -0300

    Add TwentyNineteen Theme (#161)
    * Add TwentyNineteen Theme
    
    * Tweak theme line-height

--- packages/theme-compatibility/webpack.config.js
+++ packages/theme-compatibility/webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
 		'crowdsignal-editor': './src/crowdsignal/editor.scss',
 		'leven': './src/leven/base.scss',
 		'leven-editor': './src/leven/editor.scss',
+		'twentynineteen': './src/twentynineteen/base.scss',
+		'twentynineteen-editor': './src/twentynineteen/editor.scss',
 	},
 	output: {
 		path: path.resolve( __dirname, 'dist' ),
