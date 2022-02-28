4f1a58f501630691b8e196847b8a718d4bcdce59
Christian Gastrell <cgastrell@gmail.com>
2022-02-28 16:42:42 -0300

    Add FINISH_RESOLUTION action type/creator.

--- apps/dashboard/src/data/actions.js
+++ apps/dashboard/src/data/actions.js
@@ -1,10 +1,16 @@
 /**
  * Internal dependencies
  */
-import { ASYNC_ROUTINE_DISPATCH } from './action-types';
+import { ASYNC_ROUTINE_DISPATCH, FINISH_RESOLUTION } from './action-types';
 
 export const dispatchAsync = ( apply, args = [] ) => ( {
 	type: ASYNC_ROUTINE_DISPATCH,
 	apply,
+	args,
+} );
+
+export const finishResolution = ( selectorName, args ) => ( {
+	type: FINISH_RESOLUTION,
+	selectorName,
 	args,
 } );
