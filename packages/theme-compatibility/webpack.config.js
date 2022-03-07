d57a97d5791fded640ad507189cf29a04c544666
jcheringer <jcheringer@users.noreply.github.com>
2022-03-07 18:09:20 -0300

    Add Blockbase Theme (#174)

--- packages/theme-compatibility/webpack.config.js
+++ packages/theme-compatibility/webpack.config.js
@@ -18,6 +18,8 @@ module.exports = {
 		'twentynineteen-editor': './src/twentynineteen/editor.scss',
 		'twentytwentytwo': './src/twentytwentytwo/base.scss',
 		'twentytwentytwo-editor': './src/twentytwentytwo/editor.scss',
+		'blockbase': './src/blockbase/base.scss',
+		'blockbase-editor': './src/blockbase/editor.scss',
 	},
 	output: {
 		path: path.resolve( __dirname, 'dist' ),
