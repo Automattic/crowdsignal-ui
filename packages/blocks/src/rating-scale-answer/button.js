3d0093b4da86df54e50a66cffa0281775e92c8c2
Christian Gastrell <cgastrell@gmail.com>
2022-03-10 19:10:33 -0300

    Add Rating Scale Question and Answer blocks

---
+++ packages/blocks/src/rating-scale-answer/button.js
@@ -0,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import { RawHTML } from '@wordpress/element';
+import styled from '@emotion/styled';
+
+/**
+ * Internal dependencies
+ */
+import { Button, FormCheckbox } from '../components';
+
+const ButtonContent = styled.span`
+	align-items: center;
+	display: flex;
+	overflow: hidden;
+	position: relative;
+	width: 100%;
+
+	${ FormCheckbox.className }.is-radio {
+		position: absolute;
+		left: -999px;
+	}
+`;
+
+const ButtonAnswer = ( { attributes, className, inputProps } ) => {
+	return (
+		<Button as="label" attributes={ attributes } className={ className }>
+			<ButtonContent>
+				<FormCheckbox { ...inputProps } />
+				<RawHTML>{ attributes.label }</RawHTML>
+			</ButtonContent>
+		</Button>
+	);
+};
+
+export default ButtonAnswer;
