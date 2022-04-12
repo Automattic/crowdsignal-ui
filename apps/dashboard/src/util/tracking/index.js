097ab51f0639c59895be42accde0f8420e688072
jcheringer <jcheringer@users.noreply.github.com>
2022-04-12 16:23:52 -0300

    Add frontend tracking events (#210)
    * Add tracking event for editor loaded and theme changed
    
    * Include WP userId in the Dashboard store
    
    * Update events names
    
    * Update methods names
    
    * Adjust events prefix

---
+++ apps/dashboard/src/util/tracking/index.js
@@ -0,0 +1,44 @@
+/* global localStorage */
+
+export const trackEvent = ( user, event, properties ) => {
+	if ( ! user ) {
+		return;
+	}
+
+	window._tkq = window._tkq || [];
+	window._tkq.push( [
+		'identifyUser',
+		user.partnerUserId,
+		user.profile.username,
+	] );
+	window._tkq.push( [
+		'recordEvent',
+		event,
+		{
+			user_id: user.userId,
+			...properties,
+		},
+	] );
+};
+
+export const trackEditorLoad = ( user, projectId ) => {
+	const EDITOR_LOADED_KEY = 'editor_last_loaded_date';
+	const lastEditorLoad =
+		Number( localStorage.getItem( EDITOR_LOADED_KEY ) ) || 0;
+	const lastEditorLoadDate = new Date( lastEditorLoad );
+	const currentDate = new Date().setHours( 0, 0, 0, 0 );
+
+	if ( user && lastEditorLoadDate < currentDate ) {
+		trackEvent( user, 'crowdsignal_project_editor_load', {
+			project_id: projectId,
+		} );
+		localStorage.setItem( EDITOR_LOADED_KEY, currentDate );
+	}
+};
+
+export const trackThemeChange = ( user, theme, projectId ) => {
+	trackEvent( user, 'crowdsignal_project_theme_change', {
+		theme,
+		project_id: projectId,
+	} );
+};
