b39dc99d2369fe98cc736ada62b51de1b9d69c2f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-05-05 12:39:02 +0200

    Temporarily disable editor feedback button and add remaining polyfills

--- apps/dashboard/src/components/editor/feedback-button.js
+++ apps/dashboard/src/components/editor/feedback-button.js
@@ -1,16 +1,11 @@
-/**
- * External dependencies
- */
-import { useEffect } from '@wordpress/element';
-
 const EditorFeedbackButton = () => {
-	useEffect(
-		() =>
-			window.crowdsignal.FeedbackWidget( 2765015, {
-				position: 'bottom left',
-			} ),
-		[]
-	);
+	// useEffect(
+	// 	() =>
+	// 		window.crowdsignal.FeedbackWidget( 2765015, {
+	// 			position: 'bottom left',
+	// 		} ),
+	// 	[]
+	// );
 
 	return null;
 };
