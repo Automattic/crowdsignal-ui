ad29d022a1de398be52e066440485475f17166c0
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-05-05 21:51:48 +0200

    Re-enable feedback button

--- apps/dashboard/src/components/editor/feedback-button.js
+++ apps/dashboard/src/components/editor/feedback-button.js
@@ -1,11 +1,16 @@
+/**
+ * External dependencies
+ */
+import { useEffect } from '@wordpress/element';
+
 const EditorFeedbackButton = () => {
-	// useEffect(
-	// 	() =>
-	// 		window.crowdsignal.FeedbackWidget( 2765015, {
-	// 			position: 'bottom left',
-	// 		} ),
-	// 	[]
-	// );
+	useEffect(
+		() =>
+			window.crowdsignal.FeedbackWidget( 2765015, {
+				position: 'bottom left',
+			} ),
+		[]
+	);
 
 	return null;
 };
