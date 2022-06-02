efe2f687379460c8fea2b1b47be5c8fcefba87f8
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-06-02 10:37:33 +0200

    Update Matrix Question description and example

--- packages/block-editor/src/matrix-question/index.js
+++ packages/block-editor/src/matrix-question/index.js
@@ -14,11 +14,83 @@ import EditMatrix from './edit';
 const settings = {
 	apiVersion: 1,
 	title: __( 'Matrix Question', 'block-editor' ),
-	description: __( 'Enter the matrix', 'block-editor' ),
+	description: __(
+		'Create a multiple choice grid of questions and answers.',
+		'block-editor'
+	),
 	category: 'crowdsignal-forms/form',
-	edit: EditMatrix,
+	keywords: [
+		__( 'multiple choice grid', 'block-editor' ),
+		__( 'matrix', 'block-editor' ),
+		__( 'rating', 'block-editor' ),
+		__( 'scale', 'block-editor' ),
+		__( 'table', 'block-editor' ),
+		__( 'column', 'block-editor' ),
+		__( 'row', 'block-editor' ),
+		__( 'question', 'block-editor' ),
+		__( 'answer', 'block-editor' ),
+		__( 'choice', 'block-editor' ),
+		__( 'form', 'block-editor' ),
+		__( 'likert', 'block-editor' ),
+		__( 'single-selection', 'block-editor' ),
+		__( 'multiple-selection', 'block-editor' ),
+		__( 'group', 'block-editor' ),
+	],
 	icon: <MatrixQuestionIcon />,
+	edit: EditMatrix,
 	attributes,
+	supports: {
+		html: false,
+		reusable: false,
+	},
+	example: {
+		attributes: {
+			question: __(
+				'How satisfied are you with each of the following?',
+				'block-editor'
+			),
+			columns: [
+				{
+					clientId: '1',
+					label: '😁',
+				},
+				{
+					clientId: '2',
+					label: '🙂',
+				},
+				{
+					clientId: '3',
+					label: '😐',
+				},
+				{
+					clientId: '4',
+					label: '🙁',
+				},
+				{
+					clientId: '5',
+					label: '😡',
+				},
+			],
+			rows: [
+				{
+					clientId: '1',
+					label: __( 'Overall experience', 'block-editor' ),
+				},
+				{
+					clientId: '2',
+					label: __( 'Product usability', 'block-editor' ),
+				},
+				{
+					clientId: '3',
+					label: __( 'Support interaction', 'block-editor' ),
+				},
+				{
+					clientId: '4',
+					label: __( 'Price', 'block-editor' ),
+				},
+			],
+		},
+	},
 };
 
 export default {
