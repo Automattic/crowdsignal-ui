783e4edf2ce6e5c102bbe8a149698679b9cc4fb5
João Heringer <joao.heringer@automattic.com>
2022-06-15 14:43:18 -0300

    Add clientId to block attributes when creating a template

--- apps/dashboard/src/components/new-project-wizard/templates/create-template.js
+++ apps/dashboard/src/components/new-project-wizard/templates/create-template.js
@@ -1,15 +1,22 @@
 /**
  * External dependencies
  */
+import { startsWith } from 'lodash';
 import { v4 as uuid } from 'uuid';
 
 const addBlockMetadata = ( blocks ) => {
-	return blocks.map( ( block ) => ( {
-		clientId: uuid(),
-		isValid: true,
-		...block,
-		innerBlocks: addBlockMetadata( block.innerBlocks ),
-	} ) );
+	return blocks.map( ( block ) => {
+		if ( startsWith( block.name, 'crowdsignal-forms/' ) ) {
+			block.attributes.clientId = uuid();
+		}
+
+		return {
+			clientId: uuid(),
+			isValid: true,
+			...block,
+			innerBlocks: addBlockMetadata( block.innerBlocks ),
+		};
+	} );
 };
 
 export const createTemplate = ( name, description, content, preview ) => ( {
