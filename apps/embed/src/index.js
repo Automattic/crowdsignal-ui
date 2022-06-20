c6c58405efe6cde36a199947839a9252bb5b77c6
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-06-20 18:04:03 +0200

    Clean up old test code

--- apps/embed/src/index.js
+++ apps/embed/src/index.js
@@ -1,28 +1,6 @@
 /**
- * External dependencies
- */
-
-/**
  * Internal dependencies
  */
 import CrowdsignalEmbed from './embed';
 
 window.customElements.define( 'crowdsignal-embed', CrowdsignalEmbed );
-
-// const onDocumentLoad = ( handleLoad ) => {
-//     if ( document.readyState === 'complete' ) {
-//         handleLoad();
-//     }
-
-//     document.addEventListener( 'DOMContentLoaded', () => {
-//         document.removeEventListener( 'DOMContentLoaded', arguments.callee );
-//         handleLoad();
-//     } );
-// };
-
-// const getCrowdsignalEmbeds = () =>
-//     document.querySelectorAll( '' );
-
-// const loadEmbeds = () =>
-
-// const template =
