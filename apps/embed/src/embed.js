c7cc1006e0247f5c9dcce68cb3de0099d8dc6045
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-06-20 17:10:10 +0200

    Implement a Crowdsignal Embed component

---
+++ apps/embed/src/embed.js
@@ -0,0 +1,39 @@
+/**
+ * Crowdsignal embed component.
+ */
+class CrowdsignalEmbed extends window.HTMLElement {
+	/**
+	 * Reference to the main iframe
+	 */
+	#frame;
+
+	constructor() {
+		super();
+
+		this.attachShadow( { mode: 'open' } );
+	}
+
+	connectedCallback() {
+		this.#frame = document.createElement( 'iframe' );
+		this.#frame.src = this.getAttribute( 'src' );
+
+		this.#frame.setAttribute( 'frameBorder', '0' );
+
+		this.#frame.width = '100%';
+		this.#frame.scrolling = 'no';
+
+		this.shadowRoot.appendChild( this.#frame );
+
+		window.addEventListener( 'message', ( event ) => {
+			if ( this.getAttribute( 'src' ).indexOf( event.origin ) !== 0 ) {
+				return;
+			}
+
+			if ( event.data.type === 'crowdsignal-forms-project-page-loaded' ) {
+				this.#frame.height = `${ event.data.pageHeight }px`;
+			}
+		} );
+	}
+}
+
+export default CrowdsignalEmbed;
