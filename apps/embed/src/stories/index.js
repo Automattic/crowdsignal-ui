e779d637c489da6216d83f76aa5d3071d85e852e
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-06-22 19:28:11 +0200

    Remove the preview attribute from the test embed link

--- apps/embed/src/stories/index.js
+++ apps/embed/src/stories/index.js
@@ -16,6 +16,6 @@ const wrapperStyles = {
 
 export const Default = () => (
 	<div style={ wrapperStyles }>
-		<crowdsignal-embed src="https://crowdsignal.localhost:9001/B2A9100BBAB921D0?preview=true" />
+		<crowdsignal-embed src="https://crowdsignal.localhost:9001/B2A9100BBAB921D0" />
 	</div>
 );
