e89ab787348ef6a11660ec33ac9cb6b432492df2
João Heringer <joao.heringer@automattic.com>
2022-06-30 17:07:27 -0300

    Create Question category and update question blocks

--- packages/block-editor/src/rating-scale-question/index.js
+++ packages/block-editor/src/rating-scale-question/index.js
@@ -19,7 +19,7 @@ const settings = {
 		'Ask a question and offer multiple answer options.',
 		'block-editor'
 	),
-	category: 'crowdsignal-forms/form',
+	category: 'crowdsignal-forms/question',
 	keywords: [
 		__( 'rating', 'block-editor' ),
 		__( 'question', 'block-editor' ),
