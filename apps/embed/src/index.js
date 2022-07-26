5131d9ecea3d6c12817b84b0228fcb4c0a882237
Kuba Birecki <kuba.birecki@crystalplanet.io>
2022-07-26 14:52:41 +0200

    Prevent multiple definitions of crowdsignal embed components

--- apps/embed/src/index.js
+++ apps/embed/src/index.js
@@ -4,5 +4,10 @@
 import CrowdsignalCard from './card';
 import CrowdsignalEmbed from './embed';
 
-window.customElements.define( 'crowdsignal-card', CrowdsignalCard );
-window.customElements.define( 'crowdsignal-embed', CrowdsignalEmbed );
+if ( window.customElements.get( 'crowdsignal-card' ) === undefined ) {
+	window.customElements.define( 'crowdsignal-card', CrowdsignalCard );
+}
+
+if ( window.customElements.get( 'crowdsignal-embed' ) === undefined ) {
+	window.customElements.define( 'crowdsignal-embed', CrowdsignalEmbed );
+}
