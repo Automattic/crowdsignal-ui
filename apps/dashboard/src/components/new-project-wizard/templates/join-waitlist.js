60d36a5f6f1d8480eb6f56ab1b60bb5bd757e41f
João Heringer <joao.heringer@automattic.com>
2022-08-05 10:33:14 -0300

    Update Join Waitlist template

--- apps/dashboard/src/components/new-project-wizard/templates/join-waitlist.js
+++ apps/dashboard/src/components/new-project-wizard/templates/join-waitlist.js
@@ -10,22 +10,99 @@ import { createTemplate } from './create-template';
 
 export const joinWaitlistTemplate = createTemplate(
 	__( 'Join Waitlist', 'dashboard' ),
-	__( 'Description' ),
+	__(
+		'Manage and organize a waitlist for your startup, restaurant, or any other business.'
+	),
 	[
 		[
 			{
+				name: 'core/columns',
+				isValid: true,
+				attributes: { isStackedOnMobile: true },
+				innerBlocks: [
+					{
+						name: 'core/column',
+						isValid: true,
+						attributes: [],
+						innerBlocks: [
+							{
+								name: 'core/image',
+								isValid: true,
+								attributes: {
+									align: 'left',
+									url:
+										'https://crowdsignal.files.wordpress.com/2022/08/dummylogo.png',
+									alt: 'dummylogo',
+									caption: '',
+									href:
+										'https://i2.wp.com/files.polldaddy.com/db6484d9fbabb04226a978861e4cb48b-62eb81f227d71.png',
+									width: 80,
+									height: 80,
+									sizeSlug: 'large',
+									linkDestination: 'media',
+								},
+								innerBlocks: [],
+								originalContent:
+									'<figure class="wp-block-image alignleft size-large is-resized"><a href="https://i2.wp.com/files.polldaddy.com/db6484d9fbabb04226a978861e4cb48b-62eb81f227d71.png"><img src="https://crowdsignal.files.wordpress.com/2022/08/dummylogo.png" alt="dummylogo" width="80" height="80"/></a></figure>',
+								validationIssues: [],
+							},
+						],
+						originalContent: '<div class="wp-block-column"></div>',
+						validationIssues: [],
+					},
+					{
+						name: 'core/column',
+						isValid: true,
+						attributes: { width: '20px' },
+						innerBlocks: [
+							{
+								name: 'core/spacer',
+								isValid: true,
+								attributes: { height: '100px' },
+								innerBlocks: [],
+								originalContent:
+									'<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>',
+								validationIssues: [],
+							},
+						],
+						originalContent:
+							'<div class="wp-block-column" style="flex-basis:20px"></div>',
+						validationIssues: [],
+					},
+					{
+						name: 'core/column',
+						isValid: true,
+						attributes: [],
+						innerBlocks: [
+							{
+								name: 'core/paragraph',
+								isValid: true,
+								attributes: { content: ' ', dropCap: false },
+								innerBlocks: [],
+								originalContent: '<p> </p>',
+								validationIssues: [],
+							},
+						],
+						originalContent: '<div class="wp-block-column"></div>',
+						validationIssues: [],
+					},
+				],
+				originalContent: '<div class="wp-block-columns">\n\n\n\n</div>',
+				validationIssues: [],
+			},
+			{
 				name: 'core/spacer',
 				isValid: true,
-				attributes: { height: '32px' },
+				attributes: { height: '6px' },
 				innerBlocks: [],
 				originalContent:
-					'<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>',
+					'<div style="height:6px" aria-hidden="true" class="wp-block-spacer"></div>',
 				validationIssues: [],
 			},
 			{
 				name: 'core/columns',
 				isValid: true,
-				attributes: { isStackedOnMobile: true, align: 'wide' },
+				attributes: { isStackedOnMobile: true },
 				innerBlocks: [
 					{
 						name: 'core/column',
@@ -38,11 +115,12 @@ export const joinWaitlistTemplate = createTemplate(
 								attributes: {
 									content:
 										'Enter your details to get on the waitlist for BETA access',
-									level: 2,
+									level: 3,
+									align: 'wide',
 								},
 								innerBlocks: [],
 								originalContent:
-									'<h2>Enter your details to get on the waitlist for BETA access</h2>',
+									'<h3 class="alignwide">Enter your details to get on the waitlist for BETA access</h3>',
 								validationIssues: [],
 							},
 						],
@@ -155,15 +233,9 @@ export const joinWaitlistTemplate = createTemplate(
 									label: 'Which best describes you?',
 									buttonLabel: '',
 									options: [
-										{
-											label: 'Professional artist',
-										},
-										{
-											label: 'Developer',
-										},
-										{
-											label: 'Academic researcher',
-										},
+										{ label: 'Professional artist' },
+										{ label: 'Developer' },
+										{ label: 'Academic researcher' },
 										{
 											label:
 												'Journalist or online creator',
@@ -200,8 +272,7 @@ export const joinWaitlistTemplate = createTemplate(
 						validationIssues: [],
 					},
 				],
-				originalContent:
-					'<div class="wp-block-columns alignwide">\n\n\n\n</div>',
+				originalContent: '<div class="wp-block-columns">\n\n\n\n</div>',
 				validationIssues: [],
 			},
 		],
