b959822d798fa138c10f7cef4b3bb440c6983c94
Dustin Hartzler <Dustin@HartzlerDM.com>
2022-09-16 12:39:57 -0400

    Updated phone number image file

--- packages/icons/src/phone-number.js
+++ packages/icons/src/phone-number.js
@@ -6,72 +6,12 @@ export const PhoneNumberIcon = () => (
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<rect width="24" height="24" fill="#FAFAFA" />
-		<rect
-			width="1440"
-			height="1063"
-			transform="translate(-1169 -244)"
-			fill="white"
-		/>
-		<g filter="url(#filter0_i_2187_39306)">
-			<rect
-				width="284"
-				height="879"
-				transform="translate(-13 -61)"
-				fill="white"
-			/>
-			<rect x="-13" y="-12" width="284" height="95" fill="white" />
-			<rect
-				x="7.75"
-				y="4.75"
-				width="8.5"
-				height="14.5"
-				rx="1.25"
-				stroke="#1E1E1E"
-				strokeWidth="1.5"
-			/>
-			<rect x="11" y="16" width="2" height="1.5" />
-		</g>
-		<defs>
-			<filter
-				id="filter0_i_2187_39306"
-				x="-13"
-				y="-61"
-				width="285"
-				height="879"
-				filterUnits="userSpaceOnUse"
-				colorInterpolationFilters="sRGB"
-			>
-				<feFlood floodOpacity="0" result="BackgroundImageFix" />
-				<feBlend
-					mode="normal"
-					in="SourceGraphic"
-					in2="BackgroundImageFix"
-					result="shape"
-				/>
-				<feColorMatrix
-					in="SourceAlpha"
-					type="matrix"
-					values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-					result="hardAlpha"
-				/>
-				<feOffset dx="1" />
-				<feComposite
-					in2="hardAlpha"
-					operator="arithmetic"
-					k2="-1"
-					k3="1"
-				/>
-				<feColorMatrix
-					type="matrix"
-					values="0 0 0 0 0.866667 0 0 0 0 0.866667 0 0 0 0 0.866667 0 0 0 1 0"
-				/>
-				<feBlend
-					mode="normal"
-					in2="shape"
-					result="effect1_innerShadow_2187_39306"
-				/>
-			</filter>
-		</defs>
+		<path
+			fill="#1E1E1E"
+			fillRule="evenodd"
+			d="M15 5.5H9a.5.5 0 00-.5.5v12a.5.5 0 00.5.5h6a.5.5 0 00.5-.5V6a.5.5 0 00-.5-.5zM9 4a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V6a2 2 0 00-2-2H9z"
+			clipRule="evenodd"
+		></path>
+		<path fill="#1E1E1E" d="M11 16h2v1.5h-2V16z"></path>
 	</svg>
 );
